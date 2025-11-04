8d767105fdf9fd76771c1fcc79d6e5ed67a14d7e
sedagame01 <sedagame@gmail.com>
2025-11-04 07:51:11 +0100

    proyecto retocado

--- tienda.js
+++ tienda.js
@@ -1,108 +1,105 @@
 const formulario=document.querySelector("#formProducto");
 const listaProductos =document.querySelector("#listaProductos");
+const cuerpoTabla=document.querySelector('#cuerpoTabla')
 const fragment=document.createDocumentFragment();
 
 //preguntar no funciona sin 
+//productos en local o array vacio para meter el primer producto 
+let productos = JSON.parse(localStorage.getItem("listaProductos")) || []
 
-const productos =[{
-    nombre:"manzana",
-    unidades:15
-},{nombre:"ropa",
-    unidades:2,
-},]
+//actualizar producto pasando el nombre y cantidad
+const actualizarProductos=(nombreProducto,nuevaCantidad)=>{
+    const cajaProducto = document.getElementById(nombreProducto)
+    if (cajaProducto){
+        const buscarClase=cajaProducto.querySelector('.cantidad')
+        buscarClase.textContent=nuevaCantidad     
+    }
+    localStorage.setItem("listaProductos", JSON.stringify(productos));
+}
 
 
-localStorage.setItem("listaProductos", JSON.stringify(productos));
-
-const listaDesdeLocal=localStorage.getItem("listaProductos")
 
 
+const restarProducto =(nombreProducto)=>{
+    //buscamos el producto
+    const busqueda = productos.find(element=>element.nombre===nombreProducto)
+    if (busqueda&&(busqueda.unidades>1)){
+        busqueda.unidades--;
+        actualizarProductos(nombreProducto,busqueda.unidades)
+    }else if(busqueda&&(busqueda.unidades===1)){
+        document.getElementById(nombreProducto).remove()
+        productos = productos.filter(p => p.nombre !== nombreProducto);
+        localStorage.setItem("listaProductos", JSON.stringify(productos));
+    }
+}
 
-/* añadir nuevos nuevos productos*/
+const sumaProducto=(nombreProducto)=>{
+    const busqueda = productos.find(element=>element.nombre===nombreProducto)
+    if (busqueda){
+        busqueda.unidades++
+        actualizarProductos(nombreProducto,busqueda.unidades)
+    }
+}
+
+/* aññadir los produtos a una tabla  le pasamos un obj*/
+const añadirProductoTabla=(productoNuevo)=>{
+    //crear
+    const caja=document.createElement('TR')
+    caja.id=productoNuevo.nombre;
+    const producto= document.createElement('TD')
+    const cantidad= document.createElement('TD')
+    const acciones= document.createElement('TD')
+    const añadir=document.createElement('BUTTON')
+    const restar=document.createElement('BUTTON')
+    //asigancion
+    producto.textContent=productoNuevo.nombre
+    cantidad.textContent=productoNuevo.unidades
+    cantidad.classList.add('cantidad')
+
+    añadir.addEventListener('click',()=>sumaProducto(productoNuevo.nombre))
+    restar.addEventListener('click',()=>restarProducto(productoNuevo.nombre))
+    añadir.textContent="SUMAR +"
+    restar.textContent="RESTAR -"
+    //armado
+    acciones.append(añadir,restar)
+    caja.append(producto,cantidad,acciones)
+    fragment.append(caja)
+    cuerpoTabla.append(fragment)
+
+}
+
+//pasamos al hacer clic en el boton el nombre del producto
+const  incomporarProducto=(testeo)=>{
+    //buscamos si existe ya 
+    const ejemplo= productos.find((valor)=>valor.nombre===testeo)
+if (ejemplo){
+    console.log(`el producto ${testeo} ya existe en la base de datos`)
+}else{
+     const nuevoProducto={nombre:testeo,unidades:1};
+     productos.push(nuevoProducto)
+     añadirProductoTabla(nuevoProducto)
+     localStorage.setItem("listaProductos", JSON.stringify(productos))
+}
+formulario.reset()
+console.log(productos)
+}
+   
+const cargarElementos = () => {
+    productos.forEach(producto => {
+        añadirProductoTabla(producto);
+    });
+};
+
+
+
+
+/* --------------EVENTOS---------------------- */
+/* añadir nuevos nuevos productos al hacer clic en boton*/
 formulario.addEventListener('submit',(ev)=>{
     const producto= formulario.elements["nombreProducto"].value;
     incomporarProducto(producto)
-    event.preventDefault();
+    ev.preventDefault();
 })
-
-const  incomporarProducto=(testeo)=>{
-    const ejemplo= productos.find((valor)=>valor.nombre===testeo)
-if (ejemplo){
-    return console.log("este elemento ya existe")
-}else{
-     productos.push({nombre:testeo,unidades:1})
-     añadirProductoTabla(testeo)
-}
-return console.log(productos)
-}
-
-
-/* aññadir los produtos a una tabla */
-const añadirProductoTabla=(productoNuevo)=>{
-    const caja=document.createElement('TR')
-    caja.id=productoNuevo;
-    const producto=document.createElement('TH')
-    const cantidad = document.createElement('TH')
-    const añadir=document.createElement('BUTTON')
-    const restar=document.createElement('BUTTON')
-
-    producto.textContent=`${productoNuevo}`
-    
-    añadir.textContent="SUMAR +"
-    restar.textContent="RESTAR -"
-    restar.id=restar.textContent
-    cantidad.textContent="1"
-
-    
-    caja.append(producto,restar,añadir,cantidad)
-
-    fragment.append(caja)
-
-
-    //const fila=docu
-
-    const formularioPrueba = document.querySelector("#listaProductos #tabla-productos #producto");
-    formularioPrueba.append(fragment)
-    //console.log(formularioPrueba)
-}
-
-//añadirProductoTabla("colacao")
-
-
-document.addEventListener("click",(evento)=>{
-if (evento.target.matches(`#mango button`)){
-    [{nombre,unidades}]=productos
-    
-    console.log(productos)
-
-}    
-const sumarCantidad=(productoNombre)=>{
-    productos.forEach(element => {
-        //si el atributo nombre del objeto es igual al que se recibe por parametro
-        if(element.nombre==productoNombre){
-            element.unidades++;
-            const capturaCantidad=document.querySelector(`#${element.nombre}:nth-child(5)`);
-            console.log(element.nombre)
-            console.log(capturaCantidad);
-            
-        }
-    });
-}
- 
-
-})
-
-/**PRUEBA */
-sumarCantidad("manzana")
-console.log(productos);
-/**FIN PRUEBA */
-
-
-/* restarStock.addEventListener("click", function (event) {
-    
-    console.log(event)
-});
- 
-  */
+cargarElementos()
 
 console.log(productos)
